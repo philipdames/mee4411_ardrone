ea258a22a3db8b35c034fba54cf707265530fd9f
Philip Dames <pdames@temple.edu>
2020-03-22 19:24:49 -0400

    Add metapackage and setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# Make sure packages are up to date
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavlink ros-kinetic-mav-comm python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox python-rtree python-numpy
+
+# Clone class package and drone simulator into catkin workspace
+cd ~/catkin_ws/src/
+git clone https://github.com/TempleRAIL/rotors_simulator.git
+git clone https://github.com/philipdames/mee4411_ardrone.git
+
+# Build catkin workspace
+cd ~/catkin_ws/
+catkin_make
+
