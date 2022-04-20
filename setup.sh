08eff6f98f473b3e9f1114a3505f597e32a4332f
Philip Dames <pdames@temple.edu>
2022-04-20 15:18:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies
 sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavros ros-kinetic-mavlink ros-kinetic-mav-comm python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox python-rtree python-numpy
+sudo apt-get install ros-melodic-desktop-full ros-melodic-joy ros-melodic-octomap-ros ros-melodic-mavros ros-melodic-mavlink ros-melodic-mav-comm python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-melodic-control-toolbox python-rtree python-numpy
 
 # Clone class package and drone simulator into catkin workspace
 cd ~/catkin_ws/src/
